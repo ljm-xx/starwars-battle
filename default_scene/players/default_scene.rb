ece19ad2820b13ed76b879c610143c836e368adc
Luka <lukamartin@gmail.com>
2010-10-25 19:05:27 -0500

    obiwan can move all around the screen.  neato.

--- default_scene/players/default_scene.rb
+++ default_scene/players/default_scene.rb
@@ -3,9 +3,22 @@ module DefaultScene
   def key_pressed(e)
     obiwan = scene.find("obiwan")
     key = e.keyCode
-    if key == 40
-      obiwan.style.y = obiwan.style.y.to_i + 1
+
+    x = obiwan.style.x.to_i
+    y = obiwan.style.y.to_i
+    
+    if key == 40 # down
+      y = y + 5
+    elsif key == 38 # up
+      y = y - 5
+    elsif key == 37 # left
+      x=x-5
+    elsif key == 39 # right
+      x=x+5
     end
+    
+    obiwan.style.x = x
+    obiwan.style.y = y
   end
   
 end

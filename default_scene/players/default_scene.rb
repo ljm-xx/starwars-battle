43f02056c8d9b8d74ff469e18f0a7b6f6cf7c20c
Luka <lukamartin@gmail.com>
2010-10-27 19:41:05 -0500

    obiwan stays on skren

--- default_scene/players/default_scene.rb
+++ default_scene/players/default_scene.rb
@@ -4,16 +4,21 @@ module DefaultScene
     obiwan = scene.find("obiwan")
     key = e.keyCode
 
+    min_x = 0
+    max_x = area.right- obiwan.area.width
+    min_y = 0
+    max_y = area.bottom - obiwan.area.height
+
     x = obiwan.style.x.to_i
     y = obiwan.style.y.to_i
     
-    if key == 40 # down
+    if key == 40 && y < max_y # down
       y = y + 5
-    elsif key == 38 # up
+    elsif key == 38 && y > min_y # up
       y = y - 5
-    elsif key == 37 # left
+    elsif key == 37 && x> min_x# left
       x=x-5
-    elsif key == 39 # right
+    elsif key == 39 && x< max_x# right
       x=x+5
     end
     
